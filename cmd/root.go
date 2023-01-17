69df433fd32951afe7712e08f8d5cd28e00c5d85
Abhisman <abhisman.sarkar@gmail.com>
2023-01-17 14:49:24 +0530

    Updated root.go
    Comment fixes

--- cmd/root.go
+++ cmd/root.go
@@ -73,7 +73,7 @@ func kubewrite() *cobra.Command {
 	return rootCmd
 }
 
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// This is called by main.go
 func Execute() {
 	err := kubewrite().Execute()
 	if err != nil {
